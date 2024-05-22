f08ed60e1a7fc7c1cf594a574a97250a0bfe3885
Chris McDonald <chris.mcdonald.dev@gmail.com>
2024-05-22 12:45:47 +0900

    Use an input element on the page instead of the prompt() function.

--- coupon-site/scripts/main.js
+++ coupon-site/scripts/main.js
@@ -3,12 +3,10 @@ const myImage = document.querySelector("img");
 myImage.addEventListener("click", switchCouponImage)
 
 function switchCouponImage() {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/coupon-59.png") {
-    myImage.setAttribute("src", "images/coupon-60.png");
-  } else {
-    myImage.setAttribute("src", "images/coupon-59.png");
-  }
+  const originalImage = "images/coupon-59.png";
+  const secondImage = "images/coupon-60.png";
+  // If the current image source is the original image, change it to the second image. Otherwise, change it to the original image.
+  myImage.src = myImage.src === originalImage ? secondImage : originalImage;
 }
 
 // Select the button and heading elements. We'll switch the name in the heading when the button is clicked.
@@ -18,21 +16,32 @@ let myHeading = document.querySelector("h1");
 // Add a "click" event listener to the button. Call the setUserName function when the button is clicked.
 myButton.addEventListener("click", setUserName)
 
-function setUserName() {
-  const myName = prompt("Please enter your name.");
+// Select the name input element and store the original border color. We'll later change this color to red if the user doesn't enter a name.
+const userNameInput = document.querySelector("input");
+const nameInputOriginalBorderColor = userNameInput.style.borderColor;
+
+function setUserName(event) {
+  // Form elements refresh the page by default when the user input is submitted. Prevent the form from submitting.
+  event?.preventDefault(); 
+
+  // If the name input is empty, change the border color to red and stop the function from continuing.
+  const myName = userNameInput.value;
   if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Youpons is cool, ${myName}`;
+    userNameInput.style.borderColor = "red";
+    return
   }
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Youpons is cool, ${myName}`;
+  // Reset the border color to the default color.
+  userNameInput.style.borderColor = nameInputOriginalBorderColor;
 }
   
 // When the user first visits the site, ask them for their name and store it in local storage.
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Youpons is cool, ${storedName}`;
+function onFirstVisit() {
+  const nameSavedOnPreviousVisit = localStorage.getItem("name")
+  if (!nameSavedOnPreviousVisit) {
+    setUserName();
+  }
 }
-  
+
+onFirstVisit()
